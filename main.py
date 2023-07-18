03180032e4ca3a9595b6536e96139121f6b04a7e
xingjianll <4396kevinliu@gmail.com>
2023-07-19 04:43:29 +0800

    Added connection timeout.

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
 import socket
+import threading
+import time
+
 import getresponse
 import openai
 
 HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
 PORT = 4396  # Port to listen on (non-privileged ports are > 1023)
-
-
-def set_keepalive_l(sock, after_idle_sec=3, interval_sec=5, max_fails=1):
-    """Set TCP keepalive on an open socket.
-
-    It activates after 1 second (after_idle_sec) of idleness,
-    then sends a keepalive ping once every 3 seconds (interval_sec),
-    and closes the connection after 5 failed ping (max_fails), or 15 seconds
-    """
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
-
-
-def set_keepalive_o(sock, after_idle_sec=3, interval_sec=3, max_fails=5):
-    """Set TCP keepalive on an open socket.
-
-    sends a keepalive ping once every 3 seconds (interval_sec)
-    """
-    # scraped from /usr/include, not exported by python's socket module
-    TCP_KEEPALIVE = 0x10
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
 
 
 # Press the green button in the gutter to run the script.
@@ -41,8 +20,8 @@ if __name__ == '__main__':
             conn, addr = s.accept()
 
             with conn:
+                conn.settimeout(120)
                 print(f"====Connected by {addr}======")
-                set_keepalive_o(conn, 5, 5, 1)
                 responseGetter = getresponse.ResponseGetter()
                 responseGetter.initialize()
                 responseGetter.set_apikey("")
@@ -83,9 +62,8 @@ if __name__ == '__main__':
                                     print("other exit\n\n")
                                     break
                             except TimeoutError:
-                                conn.send(bytes(
-                                    "01" + "[Connection timeout, please login again.]" + "\nEND4321\n",
-                                    'utf-8'))
+                                conn.send(bytes("01" + "[Connection timed out due to inactivity, please login again]" + "\nEND4321\n",
+                                                'utf-8'))
                                 print("connection timeout exit\n\n")
                                 break
 
